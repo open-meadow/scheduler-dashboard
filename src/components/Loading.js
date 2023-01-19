2aaa359ffab7321e945b180b0371ade535147bc2
open-meadow <rohan.varughese@protonmail.com>
2023-01-19 16:39:12 -0500

    Initial commit

---
+++ src/components/Loading.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class Loading extends Component {
+  state = { loading: true }
+
+  render() {
+    return <section className="loading">Loading</section>
+  }
+}
